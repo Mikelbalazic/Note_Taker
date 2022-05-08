fe940479f67fc7d6fc5f503b42c177a96ede1ef3
Mikel Balazic <mikelbalazic@Mikels-iMac.local>
2022-05-08 11:54:15 -0400

    Final Push

--- routes/routes.js
+++ routes/routes.js
@@ -2,30 +2,39 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
 
         if (err) throw err; 
 
         let notes = JSON.parse(data);
-
+        console.log(notes)
         app.get("/api/notes", function(req, res){
             res.json(notes);
         });
 
         app.post("/api/notes", function (req, res){
             let newestNote = req.body;
-            notes.push(newestNote);
+            let currentId = notes.length + 1;
+            notes.push({id: currentId, title: newestNote.title, text: newestNote.text});
             updateDbjson();
+            res.send("New note added!");
             return console.log("New note added:" + newestNote.title);
         });
 
-        app.get("/api/notes:id", function(req, res){
+        app.get("/api/notes/:id", function(req, res){
             res.json(notes[req.params.id]);
         });
 
-        app.delete("/api/notes:id", function(req, res){
-            notes.splice(req.params.id, 1);
+        app.delete("/api/notes/:id", function(req, res){
+            let correctId = 0;
+            for(let i = 0; i < notes.length; i++){
+                if(notes[i].id === Number(req.params.id)){
+                    correctId = i; 
+                }
+            }
+            notes.splice(correctId, 1);
             updateDbjson();
+            res.send("Note deleted!");
             console.log("Note deleted with designated id" + req.params.id);
         });
 
@@ -38,7 +47,7 @@ module.exports = app => {
         });
 
         function updateDbjson(){
-            fs.writeFile("db.db.json", JSON.stringify(notes), err =>{
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), err =>{
                 if (err) throw err;
                 return true;
             });
